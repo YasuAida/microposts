d458168d0828ff5859d2c38d111326e53351136b
YasuAida <kirakunakaishain@yahoo.co.jp>
2016-09-28 07:55:04 +0000

    revised_profile_task

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,5 +5,9 @@ class User < ActiveRecord::Base
     validates :email, presence: true, length: { maximum: 255 },
                     format: { with: VALID_EMAIL_REGEX },
                     uniqueness: { case_sensitive: false }
+    validates :profile, length: { maximum: 400 }
+    validates :email_address, length: { maximum: 255 },
+                    format: { with: VALID_EMAIL_REGEX }
+    validates :region, length: { maximum: 50 }
     has_secure_password
 end
